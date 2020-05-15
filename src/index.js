d677cf966a9705fa4b0218f4054a23e76891fe97
Bozo-max <benjamin.fargeton@hotmail.fr>
2020-05-15 19:50:45 +0200

    React OC MOOC. Memory with console log on cards click

--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// const Greeter = ({whom}) => (
+//   <button onClick={()=>{alert('coucou '+whom);}}  >
+//     Clique Clique !
+//   </button>
+// )
+// ReactDOM.render(
+//   <div>
+//     <Greeter whom='Mio'/>
+//   </div>
+//   ,
+//   document.getElementById('root')
+// )
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
