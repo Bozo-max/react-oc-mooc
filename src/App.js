c0ebed52249c33a0a491b8db53852c5f5cbd9c02
Bozo-max <benjamin.fargeton@hotmail.fr>
2020-05-16 14:27:16 +0200

    handleNewPair / handleClick / getFeedBack for card

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './App.css';
 
 const SIDE = 6
 const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MSECS = 750;
 
 class App extends Component {
   state = {
@@ -30,21 +31,46 @@ class App extends Component {
     return shuffle(result);
   }
 
-  handleCardClick = (card) => {
-    console.log(card, this)
+  handleCardClick = (index) => {
+    const { currentPair} = this.state;
+    if(currentPair.length === 0){
+      this.setState({currentPair:[index]});
+      return;
+    }
+    if(currentPair.length === 2){
+      return;
+    }
+    this.handleNewPairClosedBy(index);
   }
 
-  getFeedbackForCard(index){
-    const {currentPair, matchedCardIndices} = this.state;
-    var indexMatched = matchedCardIndices.includes(index) ? 'visible' : 'hidden';
+  handleNewPairClosedBy(index){
+    const {cards, currentPair, guesses, matchedCardIndices} = this.state;
 
-    if (currentPair.length===1 && currentPair[0]===index){
-      indexMatched = 'visible';
+    const newPair = [currentPair[0], index];
+    const newGuesses = guesses+1;
+    const matched = cards[newPair[1]] === cards[newPair[0]];
+    this.setState({currentPair: newPair, guesses: newGuesses});
+
+    if(matched){
+      this.setState({matchedCardIndices:[...matchedCardIndices, ...newPair]});
     }
-    else if (currentPair.length===2 && currentPair.includes(index)) {
-      indexMatched = matchedCardIndices.includes(index) ? 'justMatched' : 'justMismatched';
+    setTimeout(()=>this.setState({ currentPair:[] }), VISUAL_PAUSE_MSECS);
+
+  }
+
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
     }
-    return indexMatched;
+
+    if (currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+
+    return indexMatched ? 'visible' : 'hidden'
   }
 
   render(){
@@ -57,6 +83,7 @@ class App extends Component {
           <Card
             card={card}
             key = {index}
+            index={index}
             onClick = {this.handleCardClick}
             feedback = {this.getFeedbackForCard(index)}/>
         ))}
--- src/Card.js
+++ src/Card.js
@@ -4,8 +4,8 @@ import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({card,feedback, onClick}) => (
-  <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+const Card = ({card,feedback, onClick, index}) => (
+  <div className={`card ${feedback}`} onClick={() => onClick(index)}>
     <span className='symbol'>
       {feedback==="hidden" ? HIDDEN_SYMBOL : card}
     </span>
@@ -20,7 +20,8 @@ Card.propTypes = {
     'justMatched',
     'justMismatched'
   ]).isRequired,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired
 }
 
 export default Card
